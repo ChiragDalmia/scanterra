1856987290f956e63a26baefbd9f99faf4f81e50
CHIRAG DALMIA <119760552+ChiragDalmia@users.noreply.github.com>
2024-08-03 14:17:49 -0400

    added lookup keys to env

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -9,16 +9,23 @@ export async function GET(request: Request) {
     return NextResponse.json({ error: "Barcode is required" }, { status: 400 });
   }
 
+  const apiUrl = process.env.BARCODE_LOOKUP_API_URL;
+  const apiKey = process.env.BARCODE_LOOKUP_API_KEY;
+
+  if (!apiUrl || !apiKey) {
+    console.error("API URL or API Key is missing");
+    return NextResponse.json({ error: "Server configuration error" }, { status: 500 });
+  }
+
   try {
-    // const response = await fetch(
-    //   `https://api.barcodelookup.com/v3/products?barcode=${barcode}&formatted=y&key=onckwuqyzvv6maeetb4txot1tbq7l5`
-    // );
+    const response = await fetch(
+      `${apiUrl}?barcode=${barcode}&formatted=y&key=${apiKey}`
+    );
 
-    // const productData = await response.json();
-    // console.log(response);
+    const productData = await response.json();
+    console.log(productData);
 
-    // return NextResponse.json(productData);
-    return NextResponse.json({ data: "Product data" });
+    return NextResponse.json(productData);
   } catch (error) {
     console.error("Error fetching product data:", error);
     return NextResponse.json({ error: "Failed to fetch product data" }, { status: 500 });
