b5fe9cf3ac8d45bb9227736c2fb2c7469a294c34
Leo Cheng <leocheng333375749@gmail.com>
2024-08-03 22:31:52 -0400

    name section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import CTAsection from "@/components/sections/CTAsection";
 import FeaturesSection from "@/components/sections/FeaturesSection";
 import HeroSection from "@/components/sections/HeroSection";
+import NameSection from "@/components/sections/NameSection";
 export default function Home() {
   return (
     <main className="relative min-h-screen">
       <HeroSection />
       <FeaturesSection/>
       <CTAsection/>
-
+      <NameSection/>
     </main>
   );
 }
---
+++ src/components/sections/NameSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NameSection = () => {
+  return (
+    <>
+      <section className="flex h-screen flex-col justify-center px-4 py-16 text-center md:text-left">
+        
+      </section>
+    </>
+  );
+};
+
+export default NameSection;
